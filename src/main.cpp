73abde62fe45a8fe5deb54dd0bddf9e4a2b01f47
Adam Eagles <ad.c.eagles@gmail.com>
2023-11-22 21:36:37 +0000

    Web control code: JSON for storing and returning all values Toggle on and off modes return josn data to client Boost mode which  leaves heating on for an hour

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,78 @@
 #include <WiFiManager.h>  // You may need to install this library through the Arduino Library Manager
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+#include <ArduinoJson.h>
 
 ESP8266WebServer server(80);
-
+StaticJsonDocument<200> doc;
 const char *ntpServerName = "pool.ntp.org";
 const int utcOffsetInSeconds = 0; // Adjust this based on your time zone
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, ntpServerName, utcOffsetInSeconds);
+int boost_duration = 30;
+
+bool checkIsOn(){
+  // TODO - Analogue read from photoresistor
+  return true;
+}
+
+void toggle(bool On){
+  if(doc["is_on"] != On){
+  Serial.print("Turning - ");
+  Serial.println(On ? "On" : "Off");
+
+  // TODO - replace this with checkIsOn;
+  doc["is_on"] = On;
+  }
+  if (On != checkIsOn()){
+    //TODO - Toggle servo here
+    
+  }
+}
 
 void handleRoot() {
   String message = "Hello, this is the root page! ";
   Serial.println("Root");
   message += timeClient.getFormattedTime();
   server.send(200, "text/plain", message);
+}
+
+void startBoost() {
+  doc["boosting"] = true;
+  doc["boost_start"] = timeClient.getEpochTime();
+  doc["boost_remaining"] = boost_duration;
+  toggle(true);
+  String jsonString;
+  serializeJson(doc, jsonString);
+  server.send(200, "application/json", jsonString);
+}
+
+void updateBoost(){
+  if(doc["boosting"]){
+    doc["boost_remaining"] = boost_duration - (timeClient.getEpochTime() - (unsigned long int)doc["boost_start"]);
+    if (doc["boost_remaining"] <= 0){
+      doc["boosting"] = false;
+      doc["boost_remaining"] = 0;
+      toggle(false);
+    }
+  }
+  
+}
+
+void handleGet(){
+  String command = server.arg("cmd");
+  Serial.println(command);
+  if (command == "On"){
+    toggle(true);
+  } else if (command == "Off" && !doc["boosting"])
+  {
+    toggle(false);
+  }
+  
+  String jsonString;
+  serializeJson(doc, jsonString);
+  server.send(200, "application/json", jsonString);
 }
 
 void handlePost() {
@@ -38,6 +96,12 @@ void handlePost() {
 
 void setup() {
   // Start Serial for debugging purposes
+  
+  doc["boost_start"] = 0;
+  doc["is_on"] = false;
+  doc["boosting"] = false;
+  doc["boost_remaining"]=0;
+
   Serial.begin(115200);
 
   // Connect to WiFi using WiFiManager
@@ -49,7 +113,11 @@ void setup() {
   // Define server routes
   server.on("/", HTTP_GET, handleRoot);
   server.on("/post", HTTP_POST, handlePost);
+  server.on("/data", HTTP_GET, handleGet);
+  server.on("/boost", HTTP_GET, startBoost);
 
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
   // Start the server
   server.begin();
   Serial.println("HTTP server started");
@@ -58,8 +126,12 @@ void setup() {
   timeClient.setTimeOffset(utcOffsetInSeconds);
   timeClient.update();
 }
-
+int loopcount = 0;
 void loop() {
   // Handle incoming client requests
   server.handleClient();
+  if(loopcount % 50 == 0)
+    updateBoost();
+  loopcount++;
+  delay(10);
 }
